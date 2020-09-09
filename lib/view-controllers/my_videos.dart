09d905bcbea3bf251c3a3dcb90424be35544f90c
Joe Kletz <joekletz@gmail.com>
2020-09-09 12:49:41 +0100

    Video player updates

--- lib/view-controllers/my_videos.dart
+++ lib/view-controllers/my_videos.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:storytellers/database/database-helper.dart';
+import 'package:storytellers/view-controllers/video_player.dart';
 
-class MyVideos extends StatelessWidget {
+class MyVideos extends StatefulWidget {
+  @override
+  _MyVideosState createState() => _MyVideosState();
+}
+
+class _MyVideosState extends State<MyVideos> {
+  final dbHelper = DatabaseHelper.instance;
+
+  List<String> a = ["A", "B"];
+
+  void _query() async {
+    final allRows = await dbHelper.queryAllRows();
+    print('query all rows:');
+    setState(() {
+      allRows.forEach((row) => a.add(row['name']));
+    });
+  }
+
+  @override
+  void initState() {
+    _query();
+    super.initState();
+  }
+
+  _showVideo(String path) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => MyVideoPlayer(path)),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("MY VIDS"),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue[50],
+        elevation: 0,
+      ),
+      body: ListView.builder(
+          itemCount: a.length,
+          itemBuilder: (context, index) {
+            return FlatButton(
+                onPressed: () => _showVideo(a[index]),
+                child: Text("My video ${index + 1}"));
+          }),
     );
   }
 }
--- lib/view-model/role_select_view-model.dart
+++ lib/view-model/role_select_view-model.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:storytellers/database/database-helper.dart';
 
 class RoleSelectViewModel with ChangeNotifier {
   static const MethodChannel nativeCallChannel =
       const MethodChannel('com.storytellers.storytellers/nativecall');
 
+  final dbHelper = DatabaseHelper.instance;
+
   launchDeepAR() async {
     String info;
     try {
       info = await nativeCallChannel.invokeMethod("pushPop");
+      _updateDB(info);
     } on PlatformException {
       info = "Failed to push native view.";
     }
+  }
 
-    print("info $info");
+  void _updateDB(String path) async {
+    // row to update
+    Map<String, dynamic> row = {
+      DatabaseHelper.columnId: 1,
+      DatabaseHelper.columnName: path,
+      DatabaseHelper.columnAge: 32
+    };
+    final rowsAffected = await dbHelper.update(row);
+    if (rowsAffected == 0) {
+      final id = await dbHelper.insert(row);
+      print('inserted row id: $id');
+    }
+    print('updated $rowsAffected row(s)');
   }
 }
